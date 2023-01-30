f1256db4b725dae31b709e05ab11690a456dc00c
Alfurqani <syifa.alfurqoni@gmail.com>
2023-01-30 13:51:30 +0700

    neovim/default.nix : set correct path lua nvim configuration

--- neovim/default.nix
+++ neovim/default.nix
@@ -25,81 +25,70 @@
         {
           plugin = lualine-nvim;
           config = ''
-            luafile ./lua/config/lualine.lua
-            # luafile ~/.config/dotfiles/modules/users/nvim/lua/config/lualine.lua
+            luafile ~/MyHome-Flakes/neovim/lua/config/lualine.lua
           '';
         }
 
         {
           plugin = nvim-tree-lua;
           config = '' 
-            luafile ./lua/config/tree.lua
-            # luafile ~/.config/dotfiles/modules/users/nvim/lua/config/tree.lua
+            luafile ~/MyHome-Flakes/neovim/lua/config/tree.lua
           '';
         }
 
-        {
-          plugin = nvim-cmp;
-          config = ''
-            luafile ./lua/config/cmp.lua
-            # luafile ~/.config/dotfiles/modules/users/nvim/lua/config/cmp.lua
-          '';
-        }
+        # {
+        #   plugin = nvim-cmp;
+        #   config = ''
+        #     luafile ~/MyHome-Flakes/neovim/lua/config/cmp.lua
+        #   '';
+        # }
 
         {
           plugin = lspkind-nvim;
           config = ''
-            luafile ./lua/config/lspkind.lua
-            # luafile ~/.config/dotfiles/modules/users/nvim/lua/config/lspkind.lua
+            luafile ~/MyHome-Flakes/neovim/lua/config/lspkind.lua
           '';
         }
 
         {
           plugin = telescope-nvim;
           config = ''
-            luafile ./lua/config/telescope.lua
-            # luafile ~/.config/dotfiles/modules/users/nvim/lua/config/telescope.lua
+            luafile ~/MyHome-Flakes/neovim/lua/config/telescope.lua
           '';
         }
 
         {
           plugin = indent-blankline-nvim;
           config = ''
-            luafile ./lua/config/indent.lua
-            # luafile ~/.config/dotfiles/modules/users/nvim/lua/config/indent.lua
+            luafile ~/MyHome-Flakes/neovim/lua/config/indent.lua
           '';
-
         }
 
         {
           plugin = nvim-lspconfig;
           config = ''
-            luafile ./lua/config/lsp.lua
-            # luafile ~/.config/dotfiles/modules/users/nvim/lua/config/lsp.lua
+            luafile ~/MyHome-Flakes/neovim/lua/config/lsp.lua
           '';
         }
 
         {
           plugin = bufferline-nvim;
           config = ''
-            luafile ./lua/config/bufferline.lua
-            # luafile ~/.config/dotfiles/modules/users/nvim/lua/config/bufferline.lua
+            luafile ~/MyHome-Flakes/neovim/lua/config/bufferline.lua
           '';
         }
 
         {
           plugin = nvim-treesitter;
           config = ''
-            luafile ./lua/config/treesitter.lua
-            # luafile ~/.config/dotfiles/modules/users/nvim/lua/config/treesitter.lua
+            luafile ~/MyHome-Flakes/neovim/lua/config/treesitter.lua
           '';
         }
       ];
 
       extraConfig = ''
-        luafile ./lua/settings.lua
+        luafile ~/MyHome-Flakes/neovim/lua/settings.lua
         luafile ~/MyHome-Flakes/neovim/lua/maps.lua
-        # luafile ~/.config/dotfiles/modules/users/nvim/lua/maps.lua
       '';
     };
   };
