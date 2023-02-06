95befebadddefd610e15bbaf3a8640e110b76282
Alfurqani <syifa.alfurqoni@gmail.com>
2023-02-06 23:59:32 +0700

    home/neovim/lua/maps.lua : add maps for colorscheme nordfox

--- home/neovim/lua/maps.lua
+++ home/neovim/lua/maps.lua
@@ -15,6 +15,7 @@ map("n", "<C-k>", "<cmd>Lspsaga signature_help<CR>", {noremap = true})
 map("n", "gh", "<cmd>Lspsaga Lspsaga lsp_finder<CR>", {noremap = true})
 
 map("i", "jk", "<esc>", {noremap = true})
+map("n", "<C-n>", "<cmd>colorscheme nordfox<CR>", {noremap = true})
 
 vim.cmd([[let g:floaterm_keymap_toggle = '<F12>']])
 
